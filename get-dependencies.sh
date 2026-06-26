e72de9e916b332982e8e29f817e6b55c2da2130d
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-06-26 01:43:54 -0400

    DoomRunner setup

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+sudo pacman -Syu --noconfirm minizip \
+							 qt6-base \
+							 kvantum \
+							 qt6ct \
+							 lxqt-qtplugin \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+./get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+# sudo ./make-aur-package doomrunner
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+wget https://github.com/Youda008/DoomRunner/releases/download/v1.9.2/DoomRunner-1.9.2-Linux-x86_64-dynamic_exe.zip
+unzip ./DoomRunner-1.9.2-Linux-x86_64-dynamic_exe.zip -d ./AppDir/bin
